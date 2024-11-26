c507a311bbb251f912be1402c914d2666476463c
yokabalaji-palanisamy <yogabalaji.p1998@gmail.com>
2024-11-26 22:33:12 +0530

    Describe the changes you made

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,18 +12,20 @@ import { AuthModule } from './auth/auth.module';
 import { ServicesOfferModule } from './services_offer/services_offer.module';
 import { ServicesOffer } from './services_offer/entities/services_offer.entity';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mssql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10) || 1433,
+      port: parseInt(process.env.DB_PORT, 10) || 1433, // Default port 1433 if not set
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [User, Attendance, Client, ClientVist, ServicesOffer],
-      synchronize: true,
+      synchronize: true,  // Set to false in production
       autoLoadEntities: true,
       extra: {
         trustServerCertificate: true,  // Disable SSL certificate validation
@@ -36,7 +38,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
     AuthModule,
     ServicesOfferModule,
     DashboardModule,
-    AttendanceModule
+    AttendanceModule,
   ],
   controllers: [],
   providers: [],
